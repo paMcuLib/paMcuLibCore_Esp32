2a74ea6f492dee0b77fa9bb2e3bde04322382a9e
ActivePeter <1020401660@qq.com>
2021-06-19 16:59:32 +0800

    s

---
+++ _paTimer.cpp
@@ -0,0 +1,25 @@
+#include "paCoreInc/drv/paTimer.h"
+#include "esp_timer.h"
+paErr paTimer::initWithCallBack(int us, void (*callback1)())
+{
+    callback = callback1;
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = [](void *)
+        {
+            if (_G_paTimer.isOn)
+            {
+                _G_paTimer.callback();
+            }
+        },
+        /* name is optional, but may help identify the timer when debugging */
+        .name = "periodic"};
+
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+
+    // esp_timer_start_periodic(periodic_timer, 500000);
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, us));
+    isOn = true;
+
+    return E_Succ;
+}
